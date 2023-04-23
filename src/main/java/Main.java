61f93a02d57b2d1aeb223087bfc6fc136833f1f2
Gregor <gregor.fiddes@gmail.com>
2023-04-23 16:26:17 +0100

    Modifiers display on creation and tests started for Dice

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,9 @@ public class Main {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        Dice dice = new Dice();
         Scanner scanner = new Scanner(System.in);
-        /*
-        System.out.println("roll a dice");
-        String userInput = scanner.nextLine();
-        System.out.println("Result : " + userInput + " : " + dice.roll(userInput) );
-        */
-
         System.out.println("Welcome to the Dungeon!");
         System.out.println("Lets create a character");
         System.out.println("What is your name?:");
@@ -22,20 +15,28 @@ public class Main {
         System.out.println("What is your level? (1 if beginning)");
         int userLevel = scanner.nextInt();
         System.out.println("Welcome " + userName + " Let me generate your stats:");
+
         ArrayList<Integer> startingStats = new ArrayList<>();
+        Dice dice = new Dice();
         for (int i = 0; i < 6; i++){
             int startRoll = dice.rollAndKeepHighest("4d6", 3);
             startingStats.add(startRoll);
         }
-        Character player = new Character(userName, userLevel, startingStats.get(0), startingStats.get(1), startingStats.get(2), startingStats.get(3),
-                startingStats.get(4), startingStats.get(5));
+
+        Character player = new Character(userName, userLevel, startingStats.get(0), startingStats.get(1),
+                startingStats.get(2), startingStats.get(3), startingStats.get(4), startingStats.get(5));
         System.out.println("Your character: " + player);
 
-
+        Modifier modifier = new Modifier();
+        System.out.println("Your strength mod is: " + modifier.calculate(player.getStrength()));
+        System.out.println("Your dexterity mod is: " + modifier.calculate(player.getDexterity()));
+        System.out.println("Your constitution mod is: " + modifier.calculate(player.getConstitution()));
+        System.out.println("Your intelligence mod is: " + modifier.calculate(player.getIntelligence()));
+        System.out.println("Your wisdom mod is: " + modifier.calculate(player.getWisdom()));
+        System.out.println("Your charisma mod is: " + modifier.calculate(player.getWisdom()));
 
 
         }
-
 
     }
 
