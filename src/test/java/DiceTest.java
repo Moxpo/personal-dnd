752003bf188c78cd05f081eefc12f39148dd8248
Gregor <gregor.fiddes@gmail.com>
2023-04-23 21:38:09 +0100

    issues with mock test

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,18 +1,31 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class DiceTest {
 
     private Dice dice;
 
+    @Mock
+    private Dice mockedDice;
+
+    @Mock
+    private Random mockedRandom;
+
     @BeforeEach
     void Before(){
-        dice = new Dice();
-
+        mockedRandom = mock(Random.class);
+        mockedDice = mock(Dice.class);
     }
+
+
+/*  OLD TESTS
 
     @org.junit.jupiter.api.Test
     void roll1d6() {
@@ -26,6 +39,13 @@ class DiceTest {
         assertTrue((result <=8), "Less than 9");
     }
 
-
-
+*/
+    @Test
+    void roll1d20() {
+        when(mockedRandom.nextInt(20)).thenReturn(16);
+        // Set the mock to return 16 when nextInt(20) is called
+        int result = dice.roll("1d20");
+        assertEquals(17, result, "Correct");
+        // Expecting 17 because nextInt(20) returns 16, and we add 1 in roll() method
+    }
 }
