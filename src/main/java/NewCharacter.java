40be194012f69b7abdc501fc834017c67533f797
Gregor <105127647+Moxpo@users.noreply.github.com>
2023-04-25 15:14:53 +0100

    refactor - NewCharacter class created

---
+++ src/main/java/NewCharacter.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class NewCharacter {
+
+    public static void createCharacter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lets create a character");
+        System.out.println("What is your name?:");
+        String userName = scanner.nextLine();
+        System.out.println("What is your level? (1 if beginning)");
+        int userLevel = scanner.nextInt();
+        System.out.println("Welcome " + userName + " Let me generate your stats:");
+
+        ArrayList<Integer> startingStats = new ArrayList<>();
+        Dice dice = new Dice();
+        for (int i = 0; i < 6; i++){
+            int startRoll = dice.rollAndKeepHighest("4d6", 3);
+            startingStats.add(startRoll);
+        }
+
+        Character player = new Character(userName, userLevel, startingStats.get(0), startingStats.get(1),
+                startingStats.get(2), startingStats.get(3), startingStats.get(4), startingStats.get(5));
+        System.out.println("Your character: " + player);
+
+        Modifier modifier = new Modifier();
+        System.out.println("Your strength mod is: " + modifier.calculate(player.getStrength()));
+        System.out.println("Your dexterity mod is: " + modifier.calculate(player.getDexterity()));
+        System.out.println("Your constitution mod is: " + modifier.calculate(player.getConstitution()));
+        System.out.println("Your intelligence mod is: " + modifier.calculate(player.getIntelligence()));
+        System.out.println("Your wisdom mod is: " + modifier.calculate(player.getWisdom()));
+        System.out.println("Your charisma mod is: " + modifier.calculate(player.getWisdom()));
+
+
+        int playerAC = player.calculateAC(modifier);
+        System.out.println("Your character's Armor Class: " + playerAC);
+    }
+}
