baee72aaf3a3f6f65f60fe2b3a284fe741b7c2ae
Gregor <105127647+Moxpo@users.noreply.github.com>
2023-04-25 15:48:33 +0100

    welcome switch created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean validInput = false;
+        System.out.println("Welcome to the Dungeon");
 
-        NewCharacter.createCharacter();
+
+        do {
+            System.out.println("Please make a selection");
+            System.out.println("1. Create new character");
+            System.out.println("2. Load existing character");
+            int userChoice = scanner.nextInt();
+
+            switch(userChoice) {
+                case 1:
+                    validInput = true;
+                    NewCharacter.createCharacter();
+                    break;
+                case 2:
+                    validInput = true;
+                    System.out.println("two");
+                    break;
+                default:
+                    System.out.println("Invalid input, please select 1 or 2");
+            }
+        }
+        while (!validInput);
 
     }
-
-    }
-
-
+}
